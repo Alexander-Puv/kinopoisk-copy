3de9710bd9327884cdd10a5044ee8dc7bb674001
Alexander-Puv <sansano4sana@gmail.com>
2022-11-21 22:39:27 +0300

    starting

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import './styles/App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cl from '../styles/componentStyles/Header.module.scss'
+
+const Header = () => {
+  return (
+    <header className={cl.header}>
+      
+    </header>
+  )
+}
+
+export default Header
