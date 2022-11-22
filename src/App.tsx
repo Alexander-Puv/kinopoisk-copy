e8b4eed4ecf6ccad6c6367d98eccdcb66e329624
Alexander-Puv <sansano4sana@gmail.com>
2022-11-22 22:19:05 +0300

    footer and a little bit header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer'
 import Header from './components/Header'
+import MainPage from './pages/MainPage';
 import './styles/App.scss'
 
 function App() {
 
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+          <main>
+            <Routes>
+              <Route index element={<MainPage />} />
+            </Routes>
+          </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
