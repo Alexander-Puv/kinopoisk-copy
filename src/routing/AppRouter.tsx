3d4196b66278fb99394b2cf53bbd7475931fb6b4
Alexander-Puv <sansano4sana@gmail.com>
2022-12-08 15:55:09 +0300

    a better routing

--- src/routing/AppRouter.tsx
+++ src/routing/AppRouter.tsx
@@ -7,12 +7,6 @@ const AppRouter = () => {
       {publicRoutes.map(({path, element: Component}) => (
         <Route path={path} element={<Component/>} key={path} />
       ))}
-      {/* <Route path={MainRoute} element={<MainPage />} />
-      <Route path={BuyRoute} element={<BuyPage />} />
-      <Route path={PersonalRoute} element={<PersonalPage />} />
-      <Route path={ChannelsRoute} element={<ChannelsPage />} />
-      <Route path={SportRoute} element={<SportPage />} />
-      <Route path={HistoryRoute} element={<HistoryPage />} /> */}
     </Routes>
   )
 }
